ff67de1ef99f530a7c25faaea8e9127233eb93ac
alebaviera <baviera.alessandra@gmailcom>
2019-07-01 14:09:37 +0200

    files e scheletro js

---
+++ script.js
@@ -0,0 +1,19 @@
+// elemento biglietto
+var biglietto = document.getElementId('mio-id');
+console.log(biglietto);
+// dichiaro var
+var km, eta;
+// chiedo il numero di km
+km = prompt('quanti km devi percorrere?')
+
+
+// chiedo l'età
+eta = prompt('quanti anni hai?')
+
+// calcolo il costo km tot
+
+
+// calcolo eventuale sconto per età
+
+
+// visualizzo costo biglietto
