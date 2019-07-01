d4ed883e9ac4a7d3e8ae5b72c0b89e7ded605a64
alebaviera <baviera.alessandra@gmailcom>
2019-07-01 15:05:35 +0200

    costo minorenni

--- script.js
+++ script.js
@@ -2,18 +2,24 @@
 var biglietto = document.getElementById('mio-id');
 console.log(biglietto);
 // dichiaro var
-var km, eta;
+var km, eta, costotot;
 // chiedo il numero di km
-km = prompt('quanti km devi percorrere?')
+km = parseInt(prompt('quanti km devi percorrere?'));
 
 
 // chiedo l'età
-eta = prompt('quanti anni hai?')
+eta = parseInt(prompt('quanti anni hai?'));
 
 // calcolo il costo km tot
-
-
+costotot = km * 0.21;
+console.log(costotot);
 // calcolo eventuale sconto per età
-
-
+if (eta < 18){
+  biglietto = costotot - ((costotot / 100) * 20);
+}
+ if (eta > 65) {
+   biglietto = costotot - 
+ }
 // visualizzo costo biglietto
+
+biglietto.innerHTML = 'Il costo del tuo biglietto è ' + biglietto;
